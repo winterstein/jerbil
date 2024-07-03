162f04482883c8dd340bcf05cc88eeff799eeb4d
Daniel Winterstein <daniel@good-loop.com>
2024-07-03 09:52:42 +0300

    build-script

---
+++ build-deploy.sh
@@ -0,0 +1,26 @@
+# Copy the files needed to a build directory and zip it up
+# Send it to the production server
+
+PROJECT=jerbil
+
+# exit and report the failure if any command fails (from https://stackoverflow.com/questions/1378274/in-a-bash-script-how-can-i-exit-the-entire-script-if-a-certain-condition-occurs)
+exit_trap () {
+  local lc="$BASH_COMMAND" rc=$?
+  echo "Command [$lc] exited with code [$rc]"
+}
+trap exit_trap EXIT
+set -e
+
+BD=$PROJECT-build
+
+echo "Java"
+mvn-all.sh
+
+cd /home/winterwell/$PROJECT
+mvn clean
+mvn install -DskipTests
+mvn package -DskipTests
+
+# no js
+
+# ./build-deploy2.sh $PROJECT
